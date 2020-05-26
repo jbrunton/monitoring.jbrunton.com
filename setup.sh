0327de2d84b7253e5fb70cb355aa4291f54e512d
John Brunton <jbrunton@zipcar.co.uk>
2020-05-26 21:58:39 +0100

    LIST-182 add cp .env to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -e
 
+if [ "$1" != "dev" ] && [ "$1" != "prod" ]; then
+  echo "Valid usage:"
+  echo "  ./setup dev"
+  echo "  ./setup prod"
+  exit 1
+fi
+
+cp .env.$1 .env
 envsubst < ./alertmanager/config.template.yml > ./alertmanager/config.yml
